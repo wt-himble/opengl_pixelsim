aaf140f71237950359e4a49797af43948ccc7b92
Whimble <brwyand@gmail.com>
2024-07-11 09:08:06 -0300

    Got window working.

--- opengl_pixelsim/main.cpp
+++ opengl_pixelsim/main.cpp
@@ -4,8 +4,41 @@
 
 int main() {
 
-	std::cout << "Hello World";
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "THIS IS A WINDOW", NULL, NULL);
+
+	if (window == NULL) {
+
+		std::cout << "Failed to create window" << std::endl;
+		glfwTerminate();
+		return -1;
+
+	}
+
+	glfwMakeContextCurrent(window);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+
+		std::cout << "Failed to initialize GLAD" << std::endl;
+
+	}
+
+	glViewport(0, 0, 800, 600);
+
+	while (!glfwWindowShouldClose(window)) {
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+
+	}
+
+	glfwTerminate();
 	return 0;
+
 
 
 }
